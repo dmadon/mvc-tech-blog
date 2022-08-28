ee307fb24beb34aff7d65d47d318421c1a9e1d90
dmadon <deanna.madon@gmail.com>
2022-08-28 00:26:13 -0500

    added single-post view and ability to add comments to posts for logged-in users

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,5 +42,44 @@ router.get('/login',(req,res) => {
     res.render('login');
 })
 
+// GET THE SINGLE POST THE USER CLICKED ON FROM THE HOMEPAGE AND RENDER THE SINGLE POST'S PAGE
+router.get('/posts/:id',(req,res) => {
+    Post.findOne({
+        where:{
+            id: req.params.id
+        },
+        attributes:['id','title','content','user_id','created_at'],
+        include:[
+            {
+                model: Comment,
+                attributes:['id','comment_text','user_id','post_id','created_at'],
+                include:{
+                    model: User,
+                    attributes:['username']
+                }
+            },
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({message:'Post not found.'});
+            return;
+        }
+        const post = dbPostData.get({plain:true});
+
+        res.render('single-post',{
+            post,
+            loggedIn: req.session.loggedIn})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
